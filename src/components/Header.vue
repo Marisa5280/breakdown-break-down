201618a3f26a0ff56ce96f831aba26293942399d
Marisa Wyatt <risawyatt22@gmail.com>
2023-12-15 15:27:57 -0700

    Use Vue js to create task breakdown

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      title: "Break it Down, Don't Breakdown",
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #4caf50;
+  padding: 1rem;
+  text-align: center;
+  color: white;
+}
+</style>
